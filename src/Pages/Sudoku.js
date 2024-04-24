9d2ae3afbc13a55129ac29088f717353488c4468
Travis Snapp <traviswsnapp@outlook.com>
2024-04-24 19:44:14 -0400

    sudoku page created

--- src/Pages/Sudoku.js
+++ src/Pages/Sudoku.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export function Sudoku() {
   return (
     <div className="sudoku-game">
-      <h1> Lets Play Sudoku </h1>
+      <h1>Let's Play Sudoku</h1>
     </div>
   );
 }
